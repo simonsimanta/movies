71d46a4ba6728b73173dec6fb3fbb70da107b7e3
simon sarkar <simon sarkar>
2017-04-18 01:13:04 +0530

    Initial commit

---
+++ app/scripts/myscript.js
@@ -0,0 +1,22 @@
+// Pause the video when the modal is closed
+        $(document).on('click', '.hanging-close, .modal-backdrop, .modal', function (event) {
+            // Remove the src so the player itself gets removed, as this is the only
+            // reliable way to ensure the video stops playing in IE
+            $("#trailer-video-container").empty();
+        });
+        // Start playing the video whenever the trailer modal is opened
+        $(document).on('click', '.movie-tile', function (event) {
+            var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
+            var str = trailerYouTubeId;
+            var n = str.lastIndexOf("/");
+            var res = str.substr(n+1);
+            var sourceUrl = "https://www.youtube.com/embed/"+res ;
+            $("#trailer-video-container").empty().append($("<iframe></iframe>", {
+              'id': 'trailer-video',
+              'type': 'text-html',
+              'src': sourceUrl,
+              'frameborder': 0
+            }));
+        });
+        // Animate in the movies when the page loads
+        
